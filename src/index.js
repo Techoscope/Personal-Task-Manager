c95129ae6002a4d72f8ddb1dd19d7be7e43609a6
ovezova <ovezovabb@gmail.com>
2021-04-24 17:54:44 -0400

    create a user

--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 // import './index.css';
 // import App from './App';
 import NavBar from './Components/NavBar';
 import UsersList from './Components/UsersList';
+import ToDoList from './Components/ToDoList';
+import Posts from "./Components/Posts";
+import Albums from "./Components/Albums";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <NavBar />
-    <UsersList />
+    <Router>
+      <NavBar />
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/posts">
+          <Posts />
+        </Route>
+        <Route path="/todos">
+          <ToDoList/>
+        </Route>
+        <Route path="/albums">
+          <Albums />
+        </Route>
+        <Route path="/">
+          <UsersList />
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
