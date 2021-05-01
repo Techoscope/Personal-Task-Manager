a3338551b947bb106f20c1bb46b369d8791cee87
Begench76 <59942709+Begench76@users.noreply.github.com>
2021-05-01 18:43:20 -0400

    add blog posts

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import "../styles/Posts.css";
-
+import axios from 'axios'
 
 export default class Posts extends Component {
     constructor(props){
         super(props);
         this.state = {
-            posts: []
+            posts: [],
+            title:'',
+            body:''
+
         }
     }
 
@@ -22,7 +25,9 @@ export default class Posts extends Component {
     componentDidMount(){
         this.fetchPosts()
     }
-
+     handleInput = (e) => {
+ this.setState({[e.target.name]:e.target.value})
+     }
     removePost = (e) => {
         const data = {
             method: "DELETE"
@@ -31,11 +36,28 @@ export default class Posts extends Component {
         fetch("https://jsonplaceholder.typicode.com/posts" + e.target.parentElement.id, data);
         e.target.parentElement.remove()
     }
+    handleClick=()=>{
+        axios.post("https://jsonplaceholder.typicode.com/posts", {title:this.state.title,body:this.state.body})
+        .then(resp=>{
+           this.setState({posts:[...this.state.posts,resp.data]})
+        })
+    }
 
     render() {
-        
+        console.log(this.state)
         return (
             <div>
+                <label>TITLE</label>
+                <br/>
+                <input type='text' name='title'value={this.state.title} 
+                 onChange={this.handleInput}/>
+                <br/>
+                <label>BODY</label>
+                <br/>
+                <input type='text' name='body' value={this.state.body} onChange={this.handleInput} />
+                <br/> <br/>
+                <button onClick={this.handleClick}>Add</button>
+                <button>Cancel</button>
                 {this.state.posts.map((item,index)=>{
                     return (
                         <div className="postItems" id={item.id} key={index}>
